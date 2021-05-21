c5a411c9168809976c15cc36b13b40e0a6f6c4f4
Martín San José de Vicente <martin@Martins-MacBook.home>
2021-05-21 10:12:25 +0200

    estilos dashboard creados, falta probarlos

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,6 +1,124 @@
 import React from 'react';
 import CopyRight from '../components/CopyRight';
 
+// Importaremos clsx para trabajar con las clases
+import clsx from 'clsx';
+
+// MakeStyles y CassBaseLine --> Estilos con Material UI y el tema (theme) por defecto
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+// Componentes de Material UI
+
+// Iconos de Material UI
+
+// Componentes Propios
+
+
+// Definición de estilos
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+
+    root: {
+        display: 'flex'
+    },
+    // Toolbar del menú lateral (Drawer)
+    toolbar: {
+        paddingRight: 24
+    },
+    // Iconos del Toolbar
+    toolbarIcon: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar
+    },
+    // AppBar --> Barra de Navegación para desaparecer de la  pantalla
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        })
+    },
+    // AppBar --> Barra de Navegación para aparecer en pantalla
+    appBarShift: {
+        marginLeft: drawerWidth, // El ancho del Drawer
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen
+        })
+    },
+    // Separación entre elementos del AppBar
+    appBarSpacer: theme.mixins.toolbar,
+    // Botones del menú (Drawer)
+    menuButton: {
+        marginRight: 35,
+    },
+    // Botones del menú (Drawer) cuando el menú esté plegado
+    menuButtonHidden: {
+        display: 'none',
+    },
+    // Titulo de las opciones del menú
+    title: {
+        flexGrow: 1,
+    },
+    // Menu (Drawer) Abierto
+    drawerPaper: {
+        position: 'relative',
+        width: drawerWidth,
+        whiteSpace: 'nowrap',
+        transition: theme.transitions.create(['width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen
+        })
+    },
+    drawerPaperClosed: {
+        overflowX: 'hidden',
+        width: theme.spacing(7),
+        transition: theme.transitions.create(['width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        }),
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9),
+        }
+    },
+    // Paper del componente
+    paper: {
+        display: 'flex',
+        flexDirection: 'column',
+        overflow: 'auto',
+        padding: theme.spacing(2),
+    },
+    // Altura fija
+    fixedHeight: {
+        height: 240
+    },
+    // Estilos para los contenidos del DashBoard
+    content: {
+        flexGrow: 1,
+        overflow: 'auto',
+        height: '100vh'
+    },
+    // Container del Dashboard
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4)
+    }
+
+})
+)
+
+
+
+
+
+
 const DashBoard = () => {
     return (
         <div>
